1ac0b6a7d0f8f4c4ec9d48cab1f80838e8344fda
Elise Jones <02elise.jones02@gmail.com>
2023-04-10 19:53:10 -0600

    Combine data model and DOM to display a new palatte

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 //VARIABLES
 var hexData = 'ABCDEF0123456789';
 hexData = hexData.split('');
+var currentPalette = {};
+// var box1 = document.querySelector('.box-one');
+// var box2 = document.querySelector('.box-two');
+// var box3 = document.querySelector('.box-three');
+// var box4 = document.querySelector('.box-four');
+// var box5 = document.querySelector('box-five');
+var boxes = document.querySelectorAll(".common")
 
 
-
-
-
-//Functions
+//FUNCTIONS
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
@@ -16,13 +20,37 @@ function generateHexCode() {
   for(var i = 0; i < 6; i++) {
     hexcode += hexData[getRandomIndex(hexData)]
   }
-  return hexcode
+  return hexcode;
 }
 
 function createColor() {
   var color = {
     hexcode: generateHexCode(),
+    isLocked: false
+  }
+  return color;
+}
+
+function reassignBoxColors() {
+  for (var i = 0; i < 5; i++) {
+   currentPalette.push(createColor())
+  }
+}
+
+function reassignCurrentPalette() {
+  currentPalette = {
+    colorArray: reassignBoxColors(),
     id: Date.now()
   }
-  return color
+}
+
+function renderPalette() {
+  for (var i = 0; i < currentPalette.length; i++) {
+  boxes[i].style.backgroundColor = currentPalette[i].hexcode;
+  }
+}
+
+function displayPalette() {
+  reassignCurrentPalette();
+  renderPalette();
 }
